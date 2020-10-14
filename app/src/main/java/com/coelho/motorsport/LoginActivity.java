579e5c337e894313dbbbc2fcbceacc8a39df2d7c
NNCoelho <coelho301181@gmail.com>
2020-10-14 11:04:19 +0100

    Reformat and rearrange code

--- app/src/main/java/com/coelho/motorsport/LoginActivity.java
+++ app/src/main/java/com/coelho/motorsport/LoginActivity.java
@@ -9,9 +9,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -54,6 +63,94 @@ public class LoginActivity extends AppCompatActivity {
                     ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
                     startActivity(intent, options.toBundle());
                 }
+            }
+        });
+    }
+
+    // Login event
+    private Boolean validateUsername() {
+        String val = Objects.requireNonNull(username.getEditText()).getText().toString();
+
+        if (val.isEmpty()) {
+            username.setError("Field cannot be empty");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validatePassword() {
+        String val = Objects.requireNonNull(password.getEditText()).getText().toString();
+
+        if (val.isEmpty()) {
+            password.setError("Field cannot be empty");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void loginUser(View view) {
+        if (!validateUsername() | !validatePassword()) {
+            return;
+        } else {
+            isUser();
+        }
+    }
+
+    private void isUser() {
+
+        final String usernameInput = Objects.requireNonNull(username.getEditText()).getText().toString().trim();
+        final String passwordInput = Objects.requireNonNull(password.getEditText()).getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+
+        Query checkUser = reference.orderByChild("username").equalTo(usernameInput);
+
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists()) {
+                    username.setError(null);
+                    username.setErrorEnabled(false);
+                    String passwordFromDB = snapshot.child(usernameInput).child("password").getValue(String.class);
+
+                    if (Objects.equals(passwordFromDB, passwordInput)) {
+                        password.setError(null);
+                        password.setErrorEnabled(false);
+
+                        String nameFromDB = snapshot.child(usernameInput).child("name").getValue(String.class);
+                        String usernameFromDB = snapshot.child(usernameInput).child("username").getValue(String.class);
+                        String emailFromDB = snapshot.child(usernameInput).child("email").getValue(String.class);
+                        String locationFromDB = snapshot.child(usernameInput).child("location").getValue(String.class);
+                        String motorcycleFromDB = snapshot.child(usernameInput).child("motorcycle").getValue(String.class);
+
+                        Intent intent = new Intent(getApplicationContext(), UserprofileActivity.class);
+                        intent.putExtra("name", nameFromDB);
+                        intent.putExtra("username", usernameFromDB);
+                        intent.putExtra("email", emailFromDB);
+                        intent.putExtra("location", locationFromDB);
+                        intent.putExtra("motorcycle", motorcycleFromDB);
+                        intent.putExtra("password", passwordFromDB);
+                        startActivity(intent);
+                    } else {
+                        password.setError("Wrong password");
+                        password.requestFocus();
+                    }
+                } else {
+                    username.setError("No such user exist in our database");
+                    username.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
--- app/src/main/java/com/coelho/motorsport/UserprofileActivity.java
+++ app/src/main/java/com/coelho/motorsport/UserprofileActivity.java
@@ -1,14 +1,30 @@
 package com.coelho.motorsport;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class UserprofileActivity extends AppCompatActivity {
+
+    TextView fullName, userName, location, motorcycle;
+    TextInputLayout fullNameInput, emailInput, localizationInput, motorcycleInput, passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userprofile);
+
+        fullName = findViewById(R.id.profileFullName);
+        userName = findViewById(R.id.profileUsername);
+        location = findViewById(R.id.profileLocalization);
+        motorcycle = findViewById(R.id.profileMotorcycle);
+        fullNameInput = findViewById(R.id.profile_FullName);
+        emailInput = findViewById(R.id.profile_Email);
+        localizationInput = findViewById(R.id.profile_Localization);
+        motorcycleInput = findViewById(R.id.profile_Motorcycle);
+        passwordInput = findViewById(R.id.profile_Password);
     }
 }
