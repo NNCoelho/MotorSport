7a0c1bb64f2ae388486b2728338d75c3704ca587
NNCoelho <coelho301181@gmail.com>
2020-10-15 08:14:30 +0100

    Reformat and rearrange code

--- app/src/main/java/com/coelho/motorsport/UserprofileActivity.java
+++ app/src/main/java/com/coelho/motorsport/UserprofileActivity.java
@@ -2,11 +2,15 @@ package com.coelho.motorsport;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
@@ -16,10 +20,18 @@ public class UserprofileActivity extends AppCompatActivity {
     TextView fullName, userName, location, motorcycle;
     TextInputLayout fullNameInput, emailInput, motorcycleInput, passwordInput;
 
+    // GLOBAL VARIABLES TO HOLD USER DATA INSIDE THIS ACTIVITY
+    String _NAME, _USERNAME, _EMAIL, _LOCATION, _MOTORCYCLE, _PASSWORD;
+
+    // FIREBASE
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userprofile);
+
+        reference = FirebaseDatabase.getInstance().getReference("users");
 
         // HOOKS
         fullName = findViewById(R.id.profileFullName);
@@ -38,20 +50,67 @@ public class UserprofileActivity extends AppCompatActivity {
     private void showAllUserData() {
 
         Intent intent = getIntent();
-        String user_name = intent.getStringExtra("name");
-        String user_username = intent.getStringExtra("username");
-        String user_email = intent.getStringExtra("email");
-        String user_location = intent.getStringExtra("location");
-        String user_motorcycle = intent.getStringExtra("motorcycle");
-        String user_password = intent.getStringExtra("password");
+        _NAME = intent.getStringExtra("name");
+        _USERNAME = intent.getStringExtra("username");
+        _EMAIL = intent.getStringExtra("email");
+        _LOCATION = intent.getStringExtra("location");
+        _MOTORCYCLE = intent.getStringExtra("motorcycle");
+        _PASSWORD = intent.getStringExtra("password");
 
-        fullName.setText(user_name);
-        userName.setText(user_username);
-        location.setText(user_location);
-        motorcycle.setText(user_motorcycle);
-        Objects.requireNonNull(fullNameInput.getEditText()).setText(user_name);
-        Objects.requireNonNull(emailInput.getEditText()).setText(user_email);
-        Objects.requireNonNull(motorcycleInput.getEditText()).setText(user_motorcycle);
-        Objects.requireNonNull(passwordInput.getEditText()).setText(user_password);
+        fullName.setText(_NAME);
+        userName.setText(_USERNAME);
+        location.setText(_LOCATION);
+        motorcycle.setText(_MOTORCYCLE);
+        Objects.requireNonNull(fullNameInput.getEditText()).setText(_NAME);
+        Objects.requireNonNull(emailInput.getEditText()).setText(_EMAIL);
+        Objects.requireNonNull(motorcycleInput.getEditText()).setText(_MOTORCYCLE);
+        Objects.requireNonNull(passwordInput.getEditText()).setText(_PASSWORD);
+    }
+
+    public void update(View view) {
+        if (isNameChanged() || isEmailChanged() || isMotorcycleChanged() || isPasswordChanged()) {
+            Toast.makeText(this, "Data has been Updated Successfully", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Data is the Same and therefore Cannot be Updated", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isNameChanged() {
+        if (!_NAME.equals(Objects.requireNonNull(fullNameInput.getEditText()).getText().toString())) {
+            reference.child(_USERNAME).child("name").setValue(fullNameInput.getEditText().getText().toString());
+            fullName.setText(_NAME);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isEmailChanged() {
+        if (!_EMAIL.equals(Objects.requireNonNull(emailInput.getEditText()).getText().toString())) {
+            reference.child(_USERNAME).child("email").setValue(emailInput.getEditText().getText().toString());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isMotorcycleChanged() {
+        if (!_MOTORCYCLE.equals(Objects.requireNonNull(motorcycleInput.getEditText()).getText().toString())) {
+            reference.child(_USERNAME).child("motorcycle").setValue(motorcycleInput.getEditText().getText().toString());
+            motorcycle.setText(_MOTORCYCLE);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isPasswordChanged() {
+        if (!_PASSWORD.equals(Objects.requireNonNull(passwordInput.getEditText()).getText().toString())) {
+            reference.child(_USERNAME).child("password").setValue(passwordInput.getEditText().getText().toString());
+            _PASSWORD = passwordInput.getEditText().getText().toString();
+            return true;
+        } else {
+            return false;
+        }
     }
 }
